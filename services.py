4d56121dee3d8dcd533b689ce27d52f27bbf2e60
aimee-bridges <a.bridges@rgu.ac.uk>
2025-08-31 16:25:24 +0100

    Port Dictonary Update

---
+++ services.py
@@ -0,0 +1,20 @@
+# 📘 Dictionary mapping common ports to their typical services
+PORT_SERVICES = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    135: "RPC",
+    139: "NetBIOS",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+    902: "VMware",
+    912: "VMware",
+    3306: "MySQL",
+    3389: "RDP",
+    8080: "HTTP-Alt"
+}
