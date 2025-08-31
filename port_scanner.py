fee2e78f832c06ab04a576321fe0d859e9680adc
aimee-bridges <a.bridges@rgu.ac.uk>
2025-08-31 15:45:42 +0100

    Define function to scan single port on target IP

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,13 @@
-#For network connections
+#import socket module for for port scanning & network connections
 import socket
-#For concurrent scanning
+#For concurrent scanning of multiple ports
 import threading
+#Import datetime to timestamp open ports in the log file
+from datetime import datetime
+
+#Define a function to scan a single port on the target IP
+def scan_port(ip, port):
+    try:
+        #Create TCP socket using IPv4
+        sock = socket.scoket(socket.AF_INET, socket.SOCK_STREAM)
+        #Set a timeout so the scanner doesn't hang on closed ports
